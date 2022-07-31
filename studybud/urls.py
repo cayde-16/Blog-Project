99d3a6fb942c16321e56fe744556d05969b7e70c
Cade Alexander Heck <85086198+cayde-16@users.noreply.github.com>
2022-07-31 15:24:12 -0400

    Add files via upload

--- studybud/urls.py
+++ studybud/urls.py
@@ -18,6 +18,8 @@ from django.urls import path, include
 from django.http import HttpResponse
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,3 +29,4 @@ urlpatterns = [
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
