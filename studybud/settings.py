5cf64145e643d0dc9fd8aea77a92dca702bce6e2
Cade Alexander Heck <cadeheck16@gmail.com>
2023-05-11 20:20:08 -0400

    removed database

--- studybud/settings.py
+++ studybud/settings.py
@@ -93,10 +93,10 @@ WSGI_APPLICATION = 'studybud.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
