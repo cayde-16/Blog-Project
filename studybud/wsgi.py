659ee9f8330ba2fe38fb5ff884f01e9ab66c547f
Cade Alexander Heck <cadeheck16@gmail.com>
2023-08-28 21:39:52 -0400

    all G

--- studybud/wsgi.py
+++ studybud/wsgi.py
@@ -15,5 +15,3 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'studybud.settings')
 
 application = get_wsgi_application()
 application = WhiteNoise(application, root="static")
-
-app = application
