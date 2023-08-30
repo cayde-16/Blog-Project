b04bd6f80742df39526e29ddee57a5adcdc449ce
Cade Alexander Heck <cadeheck16@gmail.com>
2023-08-29 20:30:35 -0400

    allowed cors origin

--- studybud/settings.py
+++ studybud/settings.py
@@ -135,6 +135,9 @@ USE_I18N = True
 USE_TZ = True
 
 
+CORS_ALLOWED_ORIGINS = [
+    "https://membersonlybg.up.railway.app",
+]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
